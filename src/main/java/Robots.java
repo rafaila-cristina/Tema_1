e061db7abdae6499bb4cdf3f2d806356a0c1e2fb
pope-bogdan <73463641+pope-bogdan@users.noreply.github.com>
2020-12-07 01:31:42 +0200

    Revert "Filtrare dupa cuvinte (#12)" (#13)
    This reverts commit aeeadbce6ec8c645b967e57e50a830eeb6ec9d53.

---
+++ src/main/java/Robots.java
@@ -0,0 +1,23 @@
+import jdk.internal.net.http.common.Pair;
+
+public class Robots {
+    final private String _nameBot="Bot Bob";
+    private String _robotsUrl;
+    private Pair<String, String> pairs;
+
+
+    void createRobotsUrl(String _url)
+    {
+
+    }
+
+    void readRobotsTxt()
+    {
+        
+    }
+
+
+
+
+
+}
