9ab1ec45103077df0336a78142790fffc747da8f
alpatrami <73463625+alpatrami@users.noreply.github.com>
2020-12-07 02:02:08 +0200

    Robots (#15)
    * Schelet
    Adaugare schelet #1
    Signed-off-by: patra <alin.patrascu@mta.ro>
    
    * Filtrare Dupa Dimensiune (#11)
    
    Am adaugat filtrarea dupa dimensiune. %6
    
    Signed-off-by: pope <popescubogdanalx@yahoo.com~>
    
    Co-authored-by: pope <popescubogdanalx@yahoo.com~>
    
    * Filtrare dupa cuvinte (#12)
    
     Filtrare Dupa Cuvinte
    
    Am adaugat functia de filtrare dupa cuvinte atat in continut cat si in titlu. %5
    
    Signed-off-by: pope <popescubogdanalx@yahoo.com~>
    
    Co-authored-by: pope <popescubogdanalx@yahoo.com~>
    
    * Implementare
    Implementare partiala Robots.txt #1
    
    Signed-off-by: patra <alin.patrascu@mta.ro>
    
    * Schelet
    Adaugare schelet #1
    Signed-off-by: patra <alin.patrascu@mta.ro>
    
    * Schelet
    Implementare Schelet#1
    
    * Implementare
    Implementare partiala#1
    
    Signed-off-by: patra <alin.patrascu@mta.ro>
    
    Co-authored-by: pope-bogdan <73463641+pope-bogdan@users.noreply.github.com>
    Co-authored-by: pope <popescubogdanalx@yahoo.com~>

--- src/main/java/Robots.java
+++ src/main/java/Robots.java
@@ -1,23 +1,43 @@
 import jdk.internal.net.http.common.Pair;
 
+import java.net.*;
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
 public class Robots {
-    final private String _nameBot="Bot Bob";
-    private String _robotsUrl;
-    private Pair<String, String> pairs;
+    final private String _nameBot = "Bot Bob";
+    private URL _robotsUrl;
+    private Vector<String> _vDisallow;
+    private int _delay;
 
-
-    void createRobotsUrl(String _url)
-    {
-
+    void createRobotsUrl(URL _url) throws MalformedURLException {
+        _robotsUrl = new URL(_url.getProtocol(), _url.getHost(), _url.getPort(), _url.getPath() + "/robots.txt");
     }
 
-    void readRobotsTxt()
-    {
-        
+    void readRobotsTxt() throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(_robotsUrl.openStream()));
+        String line;
+
+
+        while ((line = in.readLine()) != null)
+            if (line.equals("User-agent: *"))
+                break;
+
+        while ((line = in.readLine()) != null) {
+            if (line.contains("Disallow")) {
+                StringTokenizer defaultTokenizer = new StringTokenizer(line);
+                defaultTokenizer.nextToken();
+                _vDisallow.add(defaultTokenizer.nextToken());
+            } else if (line.contains("Crawl-delay")) {
+                StringTokenizer defaultTokenizer = new StringTokenizer(line);
+                defaultTokenizer.nextToken();
+                _delay = Integer.parseInt(defaultTokenizer.nextToken());
+            }
+        }
+        if (_vDisallow.isEmpty())
+            _vDisallow.set(0, "0");
     }
-
-
-
 
 
 }
