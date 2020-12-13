4ebd94eb940c5503a46057ecf3ca31ae0e6cded4
patra <alin.patrascu@mta.ro>
2020-12-13 11:26:01 +0200

    Return String
    issue:#1
    Am facut o functie care returneaza Stringul de permisiuni.
    
    Signed-off-by: patra <alin.patrascu@mta.ro>

--- src/main/java/Robots.java
+++ src/main/java/Robots.java
@@ -1,4 +1,4 @@
-import jdk.internal.net.http.common.Pair;
+
 
 import java.net.*;
 import java.io.*;
@@ -9,14 +9,14 @@ public class Robots {
     final private String _nameBot = "Bot Bob";
     private URL _robotsUrl;
     private Vector<String> _vDisallow;
-    private int _delay;
+    private int _delay=-1;
 
     void createRobotsUrl(URL _url) throws MalformedURLException {
-        _robotsUrl = new URL(_url.getProtocol(), _url.getHost(), _url.getPort(), _url.getPath() + "/robots.txt");
+        this._robotsUrl = new URL(_url.getProtocol(), _url.getHost(), _url.getPort(), _url.getPath() + "/robots.txt");
     }
 
     void readRobotsTxt() throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(_robotsUrl.openStream()));
+        BufferedReader in = new BufferedReader(new InputStreamReader(this._robotsUrl.openStream()));
         String line;
 
 
@@ -25,19 +25,24 @@ public class Robots {
                 break;
 
         while ((line = in.readLine()) != null) {
-            if (line.contains("Disallow")) {
+            if (line.contains("Disallow:")) {
                 StringTokenizer defaultTokenizer = new StringTokenizer(line);
                 defaultTokenizer.nextToken();
-                _vDisallow.add(defaultTokenizer.nextToken());
-            } else if (line.contains("Crawl-delay")) {
+                this._vDisallow.add(defaultTokenizer.nextToken());
+            } else if (line.contains("Crawl-delay:")) {
                 StringTokenizer defaultTokenizer = new StringTokenizer(line);
                 defaultTokenizer.nextToken();
-                _delay = Integer.parseInt(defaultTokenizer.nextToken());
+                this._delay = Integer.parseInt(defaultTokenizer.nextToken());
             }
         }
         if (_vDisallow.isEmpty())
             _vDisallow.set(0, "0");
     }
+
+
+
+    Vector<String> getString() { return this._vDisallow; }
+
 
 
 }
