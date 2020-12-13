501ee80f2a42a3ed9abfff24a71c04b5d8281dfb
marius-paunescu <73463684+marius-paunescu@users.noreply.github.com>
2020-12-13 11:52:09 +0200

    Generare SiteMapTree pentru prima pagina (#2) (#26)
    Signed-off-by: marius-paunescu <mariusp_1998@yah00.com>
    
    Co-authored-by: marius-paunescu <mariusp_1998@yah00.com>

---
+++ src/main/java/SiteMap/SiteMapGenerator.java
@@ -0,0 +1,63 @@
+package SiteMap;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class SiteMapGenerator {
+    private String baseUrl;
+    private SiteMapTree siteMapTree;
+
+    public SiteMapGenerator(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public SiteMapTree GenerateSiteMap() throws IOException {
+        siteMapTree = new SiteMapTree(baseUrl);
+
+        //send request to baseUrl
+        String bodyString = "";
+        URL url = new URL(baseUrl);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setDoOutput(true);
+
+        BufferedReader bin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuffer stringBuffer = new StringBuffer();
+        String inputLine;
+
+        while ((inputLine = bin.readLine())!= null){
+            stringBuffer.append(inputLine);
+        }
+
+        bodyString = stringBuffer.toString();
+
+        List<String> urls;
+        urls = SiteMapParser.GetAllUrls(bodyString);
+
+        // for in urls
+        // create a new TreeNode with every url
+        // add every new TreeNode to siteMapTree
+        // /c/1/abcd
+        // /c/1/aef
+        // /c/1/tyu
+        // ==> (c) -> (1) -> (abcd)
+        //                -> (aef)
+        //                -> (tyu)
+
+        for (int i=0; i<urls.size(); i++){
+            siteMapTree.addNode(urls.get(i));
+        }
+
+        //TODO: generam toate URL-urile pe baza arborelui curent
+        // pentru fiecare URL creez sitemap-ul si facem merge intre sitemap-uri
+
+        return siteMapTree;
+    }
+
+}
---
+++ src/main/java/SiteMap/SiteMapParser.java
@@ -0,0 +1,58 @@
+package SiteMap;
+
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.*;
+
+public class SiteMapParser {
+    public static List<String> GetAllUrls(String htmlString){
+        List<String> result = new ArrayList<String>();
+
+        Dictionary dictionary = new Hashtable();        //dictionary
+        dictionary.put(1, "h");
+        dictionary.put(2, "/");
+        dictionary.put(3, "#");
+        dictionary.put(4, "");
+        int ok;
+
+        for (int i=0; i<htmlString.length(); i++){
+            for (int j=i; j<htmlString.length()-6; j++){
+                String subString = htmlString.substring(j, j + 6);
+                if(subString.equals("href=\"")){
+                    int k=1;
+                    while (j + k < htmlString.length() - j - k - 6){
+                        char ch = htmlString.charAt(j+k + 6);
+                        if (ch == '\"') {
+                            break;
+                        }
+                        k++;
+                    }
+
+                    String newUrl = htmlString.substring(j+7,j+6+k);
+                    int endIndex = newUrl.indexOf('?');
+                    if(endIndex > 0)
+                        newUrl = newUrl.substring(0, endIndex);
+                    ok=0;
+
+                    for (Enumeration counter = dictionary.elements(); counter.hasMoreElements();)
+                        if(newUrl.equals(counter.nextElement())) {       //verify dictionary
+                            ok=1;
+                            break;
+                        }
+
+                    if (ok==0) {     //if nothing found in dictionary then add url to result
+                        if (result.size() > 4000)     // Debugging pruposes
+                            return result;
+
+                        result.add(newUrl);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+
+}
