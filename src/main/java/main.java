4b7979f7870612e7349800b2b151ea3271108ba9
rafaila-cristina <rafaila.cristina@gmail.com>
2020-12-13 19:36:36 +0200

    Actualizare InputData si stergere fisiere irelevante
    Issue: #3
    Am actualizat InputData conform ultimelor modificari la nivelul functionalitatilor de sitemap si page download si am sters fisiere irelevante. De asemenea am adaugat un exemplu de fisier de configurare "file.conf"
    
    Signed-off-by: rafaila-cristina <rafaila.cristina@gmail.com>

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,15 +5,6 @@ import java.util.List;
 
 public class main {
     public static void main(String args[]) throws IOException {
-        SiteMapGenerator siteMapGenerator = new SiteMapGenerator();
-        List<String> siteUrls = siteMapGenerator.GenerateSiteMap( "https://wiki.mta.ro/");
-
-
-
-    //    DataDownloader tester=new DataDownloader();
-    //    tester._mDownload("http://www.google.com");
-    //    FilterData filtrare=new FilterData("D:\\anul4\\ingprog\\tema git\\Tema-laborator");
-    //    filtrare.FilterByExtension(".java");
 
         if(args.length>0)
         {
